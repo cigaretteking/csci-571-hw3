2304c8c69b1a4fa68468da81219e421ff50ce797
cigaretteking <jiayaow@usc.edu>
2025-03-09 06:07:11 -0700

    code

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,12 @@
-/// <reference types="@angular/localize" />
-
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { Tooltip } from 'bootstrap';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+bootstrapApplication(AppComponent).then(() => {
+  document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(el => {
+    new Tooltip(el);
+  });
+});
